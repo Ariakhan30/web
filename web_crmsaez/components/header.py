4c059d4580a30a0a146d6df7bcc97f7ad0af4e48
Cyphus <ariakhan30@gmail.com>
2023-11-24 14:59:16 +0100

    ajustes header

--- web_crmsaez/components/header.py
+++ web_crmsaez/components/header.py
@@ -7,17 +7,23 @@ import web_crmsaez.styles.styles as styles
 def header() -> rx.Component:
     return rx.box(
         rx.hstack(
-        rx.image(src="logo_comercial.png", width=Size.LOGO.value, border_radius=styles.BORDER_RADIUS, margin_x=Size.HEADER_SIZE.value),
-        rx.text("CRISTALERÍA MANOLO SÁEZ S.L.", color=styles.FONT_COLOR, font_size=Size.FONT_HEADER.value, font_family="ubuntu", padding_right=Size.HEADER_SIZE.value),
-        rx.image(src="Origlass_logo.png", height=Size.ORIGLASS.value, border_radius=styles.BORDER_RADIUS, margin_x=Size.HEADER_SIZE.value),
-        rx.link(rx.icon(tag="phone"),
-                " 96 675 07 96", href="tel:966750796",
-                color=styles.FONT_COLOR, font_size=Size.FONT_SIZE.value, is_external=True, padding_x=Size.HEADER_SIZE.value),
+            rx.hstack(
+                rx.image(src="logo_comercial.png", width=Size.LOGO.value, border_radius=styles.BORDER_RADIUS),
+                rx.text("CRISTALERÍA MANOLO SÁEZ S.L.", color=styles.FONT_COLOR, font_size=Size.FONT_HEADER.value, font_family="ubuntu", justify="left"),
+            ),
+            rx.hstack(    
+                rx.image(src="Origlass_logo.png", height=Size.ORIGLASS.value, border_radius=styles.BORDER_RADIUS),
+                rx.link(rx.icon(tag="phone"),
+                    " 96 675 07 96", href="tel:966750796",
+                    color=styles.FONT_COLOR, font_size=Size.FONT_SIZE.value, is_external=True),
+            justify="right",  
+            padding_right=Size.BIG.value,      
+            ),
         display="flex",
-        justify="space-evenly",
+        justify="space-between",
         
         ),
-        width="display-width",
+        width=styles.MAX_WIDTH,
         bg=styles.BG_COLOR_RIGHT,
 
     )
